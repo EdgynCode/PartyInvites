6d712663362b7da14986ba9c2c50f23ebf797194
EdgynCode <nguyenhuutan080603@gmail.com>
2023-09-22 16:16:38 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Diagnostics;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index() { return View(); }
+        [HttpGet]
+        public ViewResult RsvpForm() {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse rsp)
+        {
+            Repository.AddResponse(rsp);
+            return View("Thanks", rsp);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,13 @@
+﻿namespace PartyInvites.Models
+{
+    public class Repository
+    {
+        private static List<GuestResponse> _response = new List<GuestResponse>();
+        public static IEnumerable<GuestResponse> Responses => Responses;
+        public static void AddResponse(GuestResponse response)
+        {
+            Console.WriteLine(response);
+            _response.Add(response);
+        }
+    }
+}
